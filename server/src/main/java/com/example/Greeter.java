12341ba4cfc30a15b5e3d3525c8febc87c6ef54a
jam414 <jam414@yahoo.com>
2018-06-03 22:41:31 -0400

    dfgfd

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,7 @@ public class Greeter {
 
   }
 //something added to test
+//something added to test 1
   //TODO: Add javadoc comment
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
